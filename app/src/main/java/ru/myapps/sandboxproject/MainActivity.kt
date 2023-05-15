b3b008b7c3b836203948188872edc4afcb258d5b
NinaShkiryatova <WildForests@yahoo.com>
2023-05-15 19:51:11 +0900

    Changed background and username

--- app/src/main/java/ru/myapps/sandboxproject/MainActivity.kt
+++ app/src/main/java/ru/myapps/sandboxproject/MainActivity.kt
@@ -2,12 +2,24 @@ package ru.myapps.sandboxproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
+
+    val number = 136
+    lateinit var text: TextView
+    lateinit var background: ConstraintLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text = findViewById(R.id.text)
+        background = findViewById(R.id.background)
+        text.setText("Hello, $number!")
+        background.setBackgroundColor(getResources().getColor(R.color.red))
     }
 
-    val number = 136
+
+
 }
